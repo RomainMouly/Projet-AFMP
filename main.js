bb316979749e8d5a9bf31981c0758a6950ef2a94
jeyofdev <jgregoire.pro@gmail.com>
2020-09-30 12:35:04 +0200

    display navbar according to device size

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // burger button
 let burgerBtn = document.getElementById("btn-trigger")
+let navbar = document.getElementById("navbar-nav")
+let navLinks = navbar.getElementsByTagName("li")
+
 burgerBtn.addEventListener("click", () => {
     burgerBtn.classList.toggle("open")
+    navbar.classList.toggle("open")
 })
