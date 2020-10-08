a3a90d0c0ed06ac5ef7011aaffede5e52564880b
jeyofdev <jgregoire.pro@gmail.com>
2020-10-08 16:22:40 +0200

    add message to form

--- form.js
+++ form.js
@@ -1,11 +1,32 @@
 // form
+const queryString = window.location.search;
 const btn = document.getElementsByClassName("btn")
-btn[0].addEventListener('click', function (e) {
-    e.preventDefault;
 
+if (queryString !== '') {
+    const urlParams = new URLSearchParams(queryString);
+
+    const getPermis = urlParams.get('permis')
+    const getDate = urlParams.get('date')
+    const getLastname = urlParams.get('lastname')
+    const getFirstname = urlParams.get('firstname')
+    const getEmail = urlParams.get('email')
+    const getPhome = urlParams.get('phone')
+    const getMessage = urlParams.get('message')
+    
     const div = document.getElementById("message")
-    let alert = document.createElement("div")
-    alert.classList.add("alert")
-    alert.innerHTML = "Votre inscription a bien été prise en compte. A bientôt Moussaillon."
-    div.appendChild(alert)
-});
+    const alertSuccess = document.getElementsByClassName("alert alert-success")
+    const alertDanger = document.getElementsByClassName("alert alert-danger")
+    
+    if (getPermis === '' || getDate === '' || getLastname === '' || getFirstname === ''  || getEmail === '' || getPhome === '' || getMessage === '' ) {
+        let alert = document.createElement("div")
+        alert.classList.add("alert", "alert-danger")
+        alert.innerHTML = "Votre formulaire comporte des erreurs"
+        div.appendChild(alert)
+    } else {
+        let alert = document.createElement("div")
+        alert.classList.add("alert", "alert-success")
+        alert.innerHTML = "Votre inscription a bien été prise en compte. A bientôt Moussaillon."
+        div.appendChild(alert)
+    }
+}
+
